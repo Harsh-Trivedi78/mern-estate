0daa8eeeaf29963473e2184d35ca3df227725e1e
Harsh-Trivedi78 <trivediharsh346@gmail.com>
2025-01-24 20:00:54 +0530

    Complete Update User functionality.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useRef } from "react";
 import { useSelector } from "react-redux";
 import { useState } from 'react';
-
+import {app} from '../firebase';
+import { updateUserStart, updateUserSuccess , updateUserFailure } from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
 
 
 
@@ -17,55 +19,101 @@ import { useState } from 'react';
   // request.resource.contentType.matches('image/.*')
   // ;
 
+  export default function Profile() {
+    const fileRef = useRef(null);
+    const { currentUser ,loading , error } = useSelector((state) => state.user);
+    const [formData, setFormData] = useState({
+      username: currentUser.username,
+      email: currentUser.email,
+      avatar: currentUser.avatar
+    });
+    const [updateSuccess, setUpdateSuccess] = useState(false);
 
 
-export default function Profile() {
-  const fileRef = useRef(null);
 
-  const { currentUser } = useSelector((state) => state.user);
-  const [formData, setFormData] = useState({});
+
+
+    const dispatch = useDispatch();
+
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
+    const handleSubmit = async  (e) => {
+      e.preventDefault();
+      try {
+        dispatch(updateUserStart());
+        const res = await fetch(`/api/user/update/${currentUser._id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        if (data.success == false){
+          dispatch(updateUserFailure(data.message));
+          return;
+        }
+        dispatch(updateUserSuccess(data));
+        setUpdateSuccess(true);
+      } catch (error) {
+        dispatch(updateUserFailure(error.message));
+        
+      
+      }
+    }
   
-  return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl font-semibold text-center my-7"> Profile</h1>
-      <form className="flex flex-col gap-4">
-      <input type="file" ref={fileRef} hidden accept="image/*" />
-
-        <img onClick={() => fileRef.current.click()}
-          src={currentUser.avatar}
-          alt="Profile"
-          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2 "
-        />
-        <input
-          type="text"
-          placeholder="Username"
-          id="username"
-          className="border p-3 rounded-lg"
-        />
-        <input
-          type="text"
-          placeholder="email"
-          id="email"
-          className="border p-3 rounded-lg"
-        />
-        <input
-          type="text"
-          placeholder="password"
-          id="password"
-          className="border p-3 rounded-lg"
-        />
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
-          Update
-        </button>
-      </form>
-      <div className="flex justify-between mt-5">
-        <button className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 transition duration-200 cursor-pointer">
-          Delete account
-        </button>
-        <button className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400 transition duration-200 cursor-pointer">
-          Sign Out
-        </button>
+    // console.log(formData);
+  
+    return (
+      <div className="p-3 max-w-lg mx-auto">
+        <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input type="file" ref={fileRef} hidden accept="image/*" />
+  
+          <img
+            onClick={() => fileRef.current.click()}
+            src={currentUser.avatar}
+            alt="Profile"
+            className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+          />
+          <input
+            type="text"
+            placeholder="Username"
+            defaultValue={currentUser.username}
+            id="username"
+            className="border p-3 rounded-lg"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="email"
+            defaultValue={currentUser.email}
+            id="email"
+            className="border p-3 rounded-lg"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            id="password"
+            className="border p-3 rounded-lg"
+            onChange={handleChange}
+          />
+          <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+            {loading ? "Loading..." : "Update"}
+          </button>
+        </form>
+        <div className="flex justify-between mt-5">
+          <button className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 transition duration-200 cursor-pointer">
+            Delete account
+          </button>
+          <button className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400 transition duration-200 cursor-pointer">
+            Sign Out
+          </button>
+        </div>
+        <p className="text-green-700 mt-5">{updateSuccess ? 'Success' : 'User is Updated Succesfully'}</p>
       </div>
-    </div>
-  );
-}
+    );
+  }
