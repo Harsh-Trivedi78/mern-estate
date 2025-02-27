7bc91d741e7a8a77f24aef8ef1fb56712cf770ea
Harsh-Trivedi78 <trivediharsh346@gmail.com>
2025-02-27 20:16:31 +0530

    Add update listing functionality with authorization checks

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createListing,deleteListing } from '../controllers/listing.controller.js';
+import { createListing,deleteListing,UpdateListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/VerifyUser.js';
 
 const router = express.Router();
 
 router.post('/create',verifyToken, createListing)
 router.delete('/delete/:id',verifyToken, deleteListing) 
+router.post('/update/:id',verifyToken, UpdateListing)
+
+
+
 export default router;
