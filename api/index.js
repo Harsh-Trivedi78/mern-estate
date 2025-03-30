9e48baad4884f683bab5639c974fee4d53692f9c
Harsh-Trivedi78 <trivediharsh346@gmail.com>
2025-03-30 15:28:37 +0530

    ht

--- api/index.js
+++ api/index.js
@@ -50,6 +50,7 @@ app.get('*', (req, res) => {
 })
 
 
+
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
